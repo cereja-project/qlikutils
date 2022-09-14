9f13d14b3a0ff0c34967c32687c2b442160beb4f
kintorres <kin.mello@gmail.com>
2022-09-13 21:53:58 -0300

    Release 1.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ REQUIRED_PACKAGES = ['Pillow']
 
 setuptools.setup(
         name="qlikutils",
-        version="1.0.3",
+        version="1.0.4",
         author="Kin Torres",
         author_email="kin.mello@gmail.com",
         description="Toolkit to use with Qlik",
