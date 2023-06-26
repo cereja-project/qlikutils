34a490e3d4c8197b88fd01621871f796733232f1
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2023-06-26 10:01:21 -0300

    Fix args and --help

--- qlikutils/__main__.py
+++ qlikutils/__main__.py
@@ -3,15 +3,16 @@ from PIL import Image
 import argparse
 import os
 import sys
+import path
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Qlik Tools')
-    parser.add_argument('--project', type=str, required=True)
-    parser.add_argument('--layer', type=str, required=True, choices=['Extração', 'Transformação', 'Visualização', 'Extraction', 'Transformation', 'Visualization'])
-    parser.add_argument('--version', type=str, required=True)
-    parser.add_argument('--output_dir', type=str, required=True)
+    parser.add_argument('--project', type=str, required=True, help='project title to be displayed in the thumbnail')
+    parser.add_argument('--layer', type=str, required=True, help='layer title to be displayed in the thumbnail (e.g. "Extraction", "Transformation", "Visualization")')
+    parser.add_argument('--version', type=str, required=True, help='version title to be displayed in the thumbnail (e.g. "v1.0.0", "v2.0.0")')
+    parser.add_argument('--output_dir', type=str, required=False, default=str(path.Path(__file__).parent.parent/'output'), help='Output directory to save the thumbnail (default: "output/")')
 
     args = parser.parse_args()
 
@@ -19,7 +20,9 @@ if __name__ == "__main__":
 
     final_img = draw_title(background_img, args.project, f'{args.layer} - {args.version}')
 
-    project_name = args.project.replace(" ", "_").replace("\\", "").replace("/", "")
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+    project_name = args.project.strip().replace(" ", "_").replace("\\", "").replace("/", "")
     filepath = f'{args.output_dir}/{project_name}_{args.layer}_thumbnail_{args.version}.png'
     final_img.save(filepath)
 
