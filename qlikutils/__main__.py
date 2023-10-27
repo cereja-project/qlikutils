23f9d7c5794f702a961c69b7b45496bd092baf15
Kin Torres <kin.mello@gmail.com>
2023-10-27 12:45:50 -0300

    Update __main__.py

--- qlikutils/__main__.py
+++ qlikutils/__main__.py
@@ -19,8 +19,8 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Qlik Tools')
     parser.add_argument('--title', type=str, required=True, help='project title to be displayed in the thumbnail (max 20 characters)')
-    parser.add_argument('--subtitle', type=str, required=True, help='project subtitle to be displayed in the thumbnail (max 30 characters)')
-    parser.add_argument('--version', type=str, required=True, help='project version to be displayed in the thumbnail (max 10 characters) (e.g. "v1.0", "v2.0")')
+    parser.add_argument('--subtitle', type=str, required=False, help='project subtitle to be displayed in the thumbnail (max 30 characters)')
+    parser.add_argument('--version', type=str, required=False, help='project version to be displayed in the thumbnail (max 10 characters) (e.g. "v1.0", "v2.0")')
     parser.add_argument('--background', type=str, required=False, default=str(path.Path(__file__).parent/'assets'/'images'/'background.png'), help='Background image to be used (default: "assets/images/background.png")')
     parser.add_argument('--output_dir', type=str, required=False, default=str(path.Path(__file__).parent.parent/'output'), help='Output directory to save the thumbnail (default: "output/")')
     parser.add_argument('--title_position', type=str, required=False, default='center', choices=['center', 'left', 'right'], help='Title position (default: "center")')
