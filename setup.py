6a03df0f403536a6b18148b11102bffc18d66a8b
Kin Torres <kin.mello@gmail.com>
2023-10-27 13:25:28 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ REQUIRED_PACKAGES = ['Pillow']
 
 setuptools.setup(
     name="qlikutils",
-    version="1.1.13",
+    version="1.1.14",
     author="Kin Torres",
     author_email="kin.mello@gmail.com",
     description="Toolkit to use with Qlik",
